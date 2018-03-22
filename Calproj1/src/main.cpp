bf015c920e46da336c14809635dd9b42cd2977ae
Marisa Oliveira <up201308594@fe.up.pt>
2018-03-22 12:58:55 +0000

    Add files via upload

--- Calproj1/src/main.cpp
+++ Calproj1/src/main.cpp
@@ -1,5 +1,42 @@
+#include <string>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "Destino.h"
+#include "Graph.h"
+
+using namespace std;
+
+int main(void) {
+	ifstream ficheiro;
+	string nome;
+	string custo_alojS;
+	double custo_alojamento;
+	Graph<Destino> myGraph;
+
+	ficheiro.open("jurados.txt");
+
+	string line;
 
 
-int main(void){
+	while (getline(ficheiro, line)) {
+			istringstream i1;
+			string nome, custo_aloj;
+			i1.str(line);
+			getline(i1, nome, ' ');
+			getline(i1, custo_alojS, ';');
+
+			custo_alojamento = stod(custo_alojS);
+
+			Destino d1(nome, custo_alojamento);
+
+			myGraph.addVertex(d1);
+
+
+
+		}
+
 
 }
